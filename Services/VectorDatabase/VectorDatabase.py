3c058bf2c8b1331fddcc5de357145b7f76cea7f6
Adnan525 <adnaan525@gmail.com>
2024-09-10 12:55:03 +1000

    RAG service ready, can process 1 doc at a time

--- Services/VectorDatabase/VectorDatabase.py
+++ Services/VectorDatabase/VectorDatabase.py
@@ -1,3 +1,6 @@
+# see https://python.langchain.com/v0.1/docs/modules/data_connection/document_transformers/recursive_text_splitter/
+# see https://github.com/FullStackRetrieval-com/RetrievalTutorials/blob/main/tutorials/LevelsOfTextSplitting/5_Levels_Of_Text_Splitting.ipynb
+
 import os, sys
 sys.path.append(f"{os.path.dirname(os.path.abspath(__file__))}/../..")
 
@@ -17,10 +20,9 @@ class VectorDatabase(ServiceBase):
                  embedding_model: EmbeddingModel, # can set this to default but then for every DB service it will crete a new model 
                  chunk_size: int = 1000, 
                  chunk_overlap: int = 100,
-                 MARKDOWN_SEPARATORS: List[str] = ["\n\n", "\n", ""] # hierarchy of the splitter strings, 
-                 # see https://python.langchain.com/v0.1/docs/modules/data_connection/document_transformers/recursive_text_splitter/
-                 # see https://github.com/FullStackRetrieval-com/RetrievalTutorials/blob/main/tutorials/LevelsOfTextSplitting/5_Levels_Of_Text_Splitting.ipynb
-                 ):
+                 markdown_separators: List[str] = ["\n\n", "\n", ""], # hierarchy of the splitter strings
+                 file_path: str = os.path.join(os.path.dirname(__file__), "documents/disqualified.txt") # for testing
+                ):
         """
         Initialize the Vector Database
         Args:
@@ -33,8 +35,8 @@ class VectorDatabase(ServiceBase):
         self.chunk_size = chunk_size
         self.chunk_overlap = chunk_overlap
         self.vector_store = None # Will be FAISS object
-        self.doc_path:str = os.path.join(os.path.dirname(__file__), "documents/disqualified.txt") # for testing
-        self.MARKDOWN_SEPARATORS: List[str] = MARKDOWN_SEPARATORS
+        self.doc_path = file_path
+        self.markdown_separators = markdown_separators
         self.text_splitter: RecursiveCharacterTextSplitter = self._initialize_text_splitter()
 
         # Loading and preparing the documents, ready to be queried
@@ -45,7 +47,7 @@ class VectorDatabase(ServiceBase):
         return RecursiveCharacterTextSplitter(
             chunk_size=self.chunk_size,
             chunk_overlap=self.chunk_overlap,
-            separators=self.MARKDOWN_SEPARATORS
+            separators=self.markdown_separators
         )
 
 
@@ -54,7 +56,7 @@ class VectorDatabase(ServiceBase):
         Currently supports .pdf, .txt, and .csv files
         """
         file_extension = os.path.splitext(file_path)[1].lower()
-        
+    
         if file_extension == ".pdf":
             loader = PyPDFLoader(file_path)
         elif file_extension == ".txt":
@@ -63,7 +65,7 @@ class VectorDatabase(ServiceBase):
             loader = CSVLoader(file_path)
         else:
             raise ValueError(f"Unsupported file format: {file_extension}")
-        
+    
         docs = loader.load()
         return self.text_splitter.split_documents(docs)
 
--- utils/resource/resource_mg_util.py
+++ utils/resource/resource_mg_util.py
@@ -4,8 +4,10 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from typing import List
 
 from Services.Base import ServiceBase
+from utils.output_message_format.output_colour import print_info
 
 def call_cleanup(services: List[ServiceBase] = None):
+    print_info("Starting cleanup")
     if services is None:
         return
     for service in services:
