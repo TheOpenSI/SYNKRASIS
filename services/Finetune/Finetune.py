e27e7081520b7a4d6d078b2d56eae276d9b73e92
Adnan525 <adnaan525@gmail.com>
2024-10-23 11:28:44 +1100

    finetune trainer needs to be added

--- services/Finetune/Finetune.py
+++ services/Finetune/Finetune.py
@@ -2,12 +2,15 @@ import os
 import sys
 sys.path.append(f"{os.path.dirname(os.path.abspath(__file__))}/../..")
 
+import yaml
 from datasets import load_dataset
 from peft.peft_model import PeftModel
 from peft import (LoraConfig,
                   prepare_model_for_kbit_training,
                   get_peft_model)
+from transformers import TrainingArguments
 from typing import List, Union
+from trl import SFTTrainer, SFTConfig
 
 from services.Base import ServiceBase
 from services.LLM.HF_LLM.HF_LLM import HF_LLM
@@ -24,7 +27,8 @@ class Finetune(ServiceBase):
                  use_peft: bool = True,
                  peft_config: LoraConfig = None,
                  format_func: callable = None,
-                 dataset_text_field: str = "text"):
+                 dataset_text_field: str = "text",
+                 training_args_path = "config_files/finetune_config.yaml"):
         """
         Initialise the Finetune service with base model.
         Either pass the HF_LLM model or model_name to load the model.
@@ -54,18 +58,25 @@ class Finetune(ServiceBase):
         self.use_peft = use_peft
         self.peft_config = peft_config
         self.target_modules = target_modules
+        self.dataset_text_field = dataset_text_field
+        self.training_args_path = training_args_path
         self._apply_dataset_formatting_function(format_func)
         
         
     def _apply_dataset_formatting_function(self, format_func: callable) -> None:
         """
-        Apply the dataset formatting function if provided
+        Apply the dataset formatting function if provided.
+        This will ideally make the "text" field ready for training.
         """
         if format_func:
             self.dataset = self.dataset.map(format_func, batched = True)
     
     
     def _get_peft_config(self) -> Union[LoraConfig, None]:
+        # https://discuss.huggingface.co/t/task-type-parameter-of-loraconfig/52879/6
+        # https://github.com/huggingface/peft/blob/v0.8.2/src/peft/utils/peft_types.py#L68-L73
+        # https://medium.com/@tom_21755/understanding-causal-llms-masked-llm-s-and-seq2seq-a-guide-to-language-model-training-d4457bbd07fa
+        
         """
         Return the PEFT config or create a default config with r = 32 and lora_alpha = 64
         """
@@ -107,3 +118,25 @@ class Finetune(ServiceBase):
         else:
             print_warning("PEFT is disabled. Using the base model.")
             return self.model
+        
+    
+    def _load_finetune_training_args(self) -> dict:
+        """
+        Load the training arguments from the config file and returns as a dictionary
+        """
+        with open(self.training_args_path, "r") as file:
+            training_args = yaml.safe_load(file)
+        
+        return training_args
+        
+        
+    def _get_training_args(self) -> TrainingArguments:
+        """
+        Get the TrainingArguments for SFTTrainer
+
+        Returns:
+            TrainingArguments: SFTTrainer args, reads from a yaml config file.
+        """
+        trainer = SFTTrainer()
+        sfttrainer_args = self._load_finetune_training_args()
+        return TrainingArguments(**sfttrainer_args)
