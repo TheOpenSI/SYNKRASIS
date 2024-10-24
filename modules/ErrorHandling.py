d3d06bb7a328f4556fc708e5f8e46a44a5c8d8b4
Adnan525 <adnaan525@gmail.com>
2024-10-24 15:50:37 +1100

    error handling test cases added and passed

--- modules/ErrorHandling.py
+++ modules/ErrorHandling.py
@@ -2,13 +2,13 @@ import os, sys
 sys.path.append(f"{os.path.dirname(os.path.abspath(__file__))}/..")
 
 import re
-from typing import Union
+from typing import Union, Tuple
 
 from utils.output_message_format.output_colour import print_warning
 
 class ErrorHandling():
-    def __init__(self):
-        pass
+    def __init__(self, target_file_name: str = "/usr/src/app/main.py"):
+        self.target_file_name = target_file_name
     
     
     @staticmethod
@@ -23,10 +23,10 @@ class ErrorHandling():
         Returns:
         - str: The error type.
         """
-        e_m = ErrorHandling.remove_multithred_generic_error(error_message)
+        e_m = ErrorHandling.remove_multithred_generic_error(error_message).strip()
         error_type = e_m.split("\n")[-1].split(":")[0]
         
-        return error_type
+        return error_type.strip()
 
     
     
@@ -50,7 +50,7 @@ class ErrorHandling():
             return removed_generic_error_message.group()
         else:
             print_warning("Regex pattern did not match for removing generic error message.")
-            return error_message
+            return error_message.strip()
         
         
     @staticmethod
@@ -69,10 +69,28 @@ class ErrorHandling():
         - str: The error message with the external file error message removed.
         """
         target_file_name = target_file_name.replace(".", "\.")
-        remove_external_file_pattern = r"%s.*" % target_file_name
+        remove_external_file_pattern = r'File\s"%s.*' % target_file_name
         file_specific_error_message = re.search(remove_external_file_pattern, error_message, re.DOTALL).group()
         
-        return file_specific_error_message
+        return file_specific_error_message.strip()
+    
+    
+    def remove_generic_and_external_file_error(self, error_message: str) -> Tuple[str, str]:
+        """
+        Removes the generic error message and the external file error message.
+        
+        Args:
+        - error_message (str): The error message.
+        
+        Returns:
+        - str: Error Type
+        - str: Error Message removed of generic and external file error message.
+        """
+        error_message = self.remove_multithred_generic_error(error_message)
+        error_message = self.remove_external_file_error(error_message, self.target_file_name)
+        error_type = self.get_error_type(error_message)
+        
+        return error_type, error_message
     
     
     def _get_test_case(self, error_message: str, 
@@ -129,22 +147,39 @@ class ErrorHandling():
         Returns:
         - str: The assertion error message.
         """
+        _, error_message = self.remove_generic_and_external_file_error(error_message)
         if extract_test_case:
-            test_case = self._get_test_case(error_message, change_test_case_entry, to_replace, entry_point)
-            return (f"Your generated code failed the following test case - {test_case}. "
-                    "Please update the function logic so all test cases can pass. Error message added for your reference.\n"
+            test_case = self._get_test_case(error_message, change_test_case_entry, to_replace, entry_point).strip()
+            return (f"Your generated code failed the following test case - {test_case}.\n"
+                    "Please update the function logic so all test cases will pass. Error message added for your reference - \n"
                     f"{error_message}")
         else:
-            return (f"Your generated code failed a test case. "
-                    "Please update the function logic so all test cases can pass. Error message added for your reference.\n"
+            return (f"Your generated code failed a test case.\n"
+                    "Please update the function logic so all test cases will pass. Error message added for your reference - \n"
                     f"{error_message}")
+            
+            
+    def _name_error_line(self, error_message: str) -> str:
+        """
+        For NAME ERROR only.
+        Get the name error line only.
+        
+        Args:
+        - error_message (str): The error message.
+        
+        Returns:
+        - str: single error line.
+        """
+        error_message_lines = error_message.split("\n")
+        
+        return error_message_lines[-1].strip()
             
             
     def name_error_prompt(self, error_message: str) -> str:
         """
         Example: Your generated code had a NameError. 
-        Please check the function, varibale names and make sure they are same as instruction.
-        {error_message}
+        Please check the function, varibale names in your generated code and make sure they are same as instruction.
+        Error message added for your reference - {error_message}
         
         Args:
         - error_message (str): The error message.
@@ -154,13 +189,14 @@ class ErrorHandling():
         """
         error_message = self.remove_multithred_generic_error(error_message)
         error_message = self.remove_external_file_error(error_message)
+        error_message = self._name_error_line(error_message) # passing only the error line, comment this line to pass the full error message
         
-        return (f"Your generated code had a NameError. "
-                "Please check the error message for more details.\n"
-                f"{error_message}")
+        return (f"Your generated code had a NameError.\n"
+                "Please check the function, variable names in your generated code and make sure they are same as instruction.\n"
+                f"Error message added for your reference - {error_message}")
     
     
-    def generic_error_prompt(self, error_message: str) -> str:
+    def generic_error_prompt(self, error_message: str, send_original: bool = False) -> str:
         """
         Example: Your generated code had a/an {error_type}. Please check the error message for more details.
         {error_message}
@@ -176,10 +212,11 @@ class ErrorHandling():
         Returns:
         - str: The generic error message.
         """
-        error_message = self.remove_multithred_generic_error(error_message)
-        error_message = self.remove_external_file_error(error_message)
-        error_type = self.get_error_type(error_message)
+        error_type, error_message_relevant = self.remove_generic_and_external_file_error(error_message)
         
-        return (f"Your generated code had a/an {error_type}. "
-                "Please check the error message for more details.\n"
-                f"{error_message}")
+        if send_original:
+            error_message_relevant = error_message
+        
+        return (f"Your generated code had a/an {error_type}.\n"
+                "Please check the following error message for more details - \n"
+                f"{error_message_relevant}")
