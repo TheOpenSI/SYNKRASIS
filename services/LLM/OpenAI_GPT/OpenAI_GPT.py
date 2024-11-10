a6847f54b5fcfcd481348ec04e0e2f4ec266c376
Adnan525 <adnaan525@gmail.com>
2024-11-10 15:24:40 +1100

    human eval all test cases added

--- services/LLM/OpenAI_GPT/OpenAI_GPT.py
+++ services/LLM/OpenAI_GPT/OpenAI_GPT.py
@@ -11,7 +11,7 @@ from utils.output_message_format.output_colour import print_error, print_info, p
 
 class OpenAI_GPT(ServiceBase, LLMBase):
     def __init__(self,
-                 temperature: float = 0,
+                 temperature: float = 0.7,
                  seed: int = 42, 
                  model_name: str = "gpt-3.5-turbo",
                  enable_chat_history: bool = False):
