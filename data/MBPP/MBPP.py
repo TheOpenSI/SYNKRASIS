5281a590a46c1fa543fbcb8b121c90d96bb3b243
Adnan525 <adnaan525@gmail.com>
2025-02-28 13:21:52 +1100

    added mbpp data and changed system prompt to remove test cases

---
+++ data/MBPP/MBPP.py
@@ -0,0 +1,113 @@
+import os
+import sys
+sys.path.append(f"{os.path.dirname(os.path.abspath(__file__))}/../..")
+
+import json
+import re
+import pandas as pd
+import ast
+from typing import Dict, Optional, List
+from deprecated import deprecated
+
+from utils.output_message_format.output_colour import print_warning, print_success
+from modules.SignatureConverter import SignatureConverter
+from data.DatasetBase import DatasetBase
+
+class MBPP(DatasetBase):
+    def __init__(self, 
+                 file_path: str = os.path.join(os.path.dirname(os.path.abspath(__file__)), "mbpp_main.jsonl"),
+                 subset_size: Optional[int] = None):
+        """
+        Initialize the MBPP dataset loader.
+
+        Args:
+            file_path (str): Path to the JSONL file containing the dataset.
+                             Defaults to 'mbpp.jsonl' in the current directory.
+        """
+        super().__init__(file_path, subset_size)
+        self.solved_count: int = 0
+        self.unsolved_count: int = 0
+        self.results: List[Dict[int, int, str]] = [] # task_id, fix_mode_attempt_count, status 
+
+
+    def _load_data(self) -> None:
+        """
+        Load the JSON data from the file.
+        """
+        self._load_json_data()
+
+
+    @deprecated("Use process() instead for release.")
+    def next(self) -> Optional[Dict[str, str]]:
+        """
+        Return the next datapoint.
+        
+        Returns:
+            Optional[Dict[str, str]]: The next datapoint if available, else None.
+        """
+        if self.current_index < len(self.data):
+            datapoint = self.data[self.current_index]
+            self.current_index += 1
+            return self.process(datapoint)
+        
+        else:
+            print_warning("No more datapoints available")
+            return None
+
+
+    def reset(self) -> None:
+        """
+        Reset all the fields.
+        """
+        super().reset()
+        self.solved_count = 0
+        self.unsolved_count = 0
+        self.results = []
+    
+
+    def log_to_csv(self, model_name: str) -> None:
+        """
+        Write result data to CSV file using Pandas.
+        For MBPP task results.
+        Args:
+            model_name (str): Name of the LLM model used to generate the CSV file.
+        """
+        df = pd.DataFrame(self.results, columns=["task_id", "fix_mode_attempt_count", "status"])
+        df.to_csv(f"experiment_results/{model_name}_mbpp_results.csv", index = False)
+        print_success(f"Results saved to {model_name}_results.csv")
+        
+        
+    def process(self, data_point: dict) -> dict:
+        """
+        Process the data point to omit unnecessary fields.
+
+        Returns:
+            dict : Processed data point.
+        """
+        # Example function call from test case
+        function_call = self.regex_extractor.extract_function_call_from_test_case(data_point["test_list"][0])
+        
+        # Patch: Data point 768 and 926 have function calls starting with "("
+        function_call = self.extractor.remove_leading_bracket(function_call)
+        
+        # Remove the type casting from function call if present, e.g. int(foo(1, 2))
+        function_call = self.regex_extractor.extract_function_call_from_type_cast(function_call)
+        
+        # Extracting the actual function name from test_case
+        function_name = self.extractor.get_function_name_from_call(function_call) + "()"
+        
+        # Function signature
+        function_signature = SignatureConverter.convert(call_string=function_call)
+        
+        # Enforce function signature prompt
+        function_signature_prompt = (f"### Required function name for your refernce **'{function_name}'**.\n"
+                                     f"### Function Signature for your reference - {function_signature}\n"
+                                     f"### An example function call from private test cases - {function_call}\n") 
+        
+        return {
+            "task_id": data_point["task_id"],
+            "prompt": data_point["text"] + "\n" + function_signature_prompt,
+            "test_list": data_point["test_list"],
+            "function_name": function_name,
+            "function_signature": function_signature,
+            "function_call": function_call}
