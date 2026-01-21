7ff8af10f0e987bac80a54fcae913a0009f0ab24
Adnan525 <adnaan525@gmail.com>
2026-01-22 10:02:55 +1100

    dehallucinator updated

--- Agents/WorkflowDev/WorkflowDev.py
+++ Agents/WorkflowDev/WorkflowDev.py
@@ -4,17 +4,21 @@ from services.LLM.Ollama.OllamaClient import OllamaClient
 
 class WorkflowDev:
     def __init__(self):
+        # requirements analyst
         self.requirement_analyst = OllamaClient(model_name="qwen2.5-coder:latest",
                                                 container_name="localhost",
                                                 enable_chat_history=True,
                                                 max_history=1)
-        self.dehallucinator_rq = OllamaClient(model_name="qwen2.5-coder:latest",
+        # dehallucinator
+        self.dehallucinator = OllamaClient(model_name="qwen2.5-coder:latest",
                                               container_name="localhost")
+        # task decomposer
         self.task_decomposer = OllamaClient(model_name="qwen2.5-coder:latest",
                                             container_name="localhost")
         
+        # system prompts
         self.requirement_analyst.set_system_prompt_from_file("Agents/WorkflowDev/prompts/requirement_analyst.txt")
-        self.dehallucinator_rq.set_system_prompt_from_file("Agents/WorkflowDev/prompts/dehallucination_rq.txt")
+        self.dehallucinator.set_system_prompt_from_file("Agents/WorkflowDev/prompts/dehallucination.txt")
         self.task_decomposer.set_system_prompt_from_file("Agents/WorkflowDev/prompts/task_decomposer.txt")
         
 
@@ -26,16 +30,17 @@ if __name__ == "__main__":
                            "Auto-reject if score < 600. Email customer with result.")
 
     workflow_dev = WorkflowDev()
+    
     req_analyst_response = workflow_dev.requirement_analyst.generate_response(client_requirements)
-    dehallucinator_rq_response = workflow_dev.dehallucinator_rq.generate_response(client_requirements + "\n\n" +
+    dehallucinator_response = workflow_dev.dehallucinator.generate_response(client_requirements + "\n\n" +
                                                                               "High-level workflow structure:\n" +
                                                                               req_analyst_response)
     
     # refined
     req_analyst_response = workflow_dev.requirement_analyst.generate_response((
-                                                        "Your assistant requested some clarifications. " +
-                                                        "Please provide answers to the following:\n" +
-                                                        dehallucinator_rq_response))
+                                                        "Your project manager requested some clarifications. " +
+                                                        "Please address the following:\n" +
+                                                        dehallucinator_response))
     
-    decomposed_tasks = workflow_dev.task_decomposer.generate_response(client_requirements + "\n\n" +
-                                                                      req_analyst_response)
+    # decomposed_tasks = workflow_dev.task_decomposer.generate_response(client_requirements + "\n\n" +
+    #                                                                   req_analyst_response)
